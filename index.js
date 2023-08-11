1f2193117804d78336ba1707d8be24fcf6e2e2bf
Jake Koenig <jakethekoenig@gmail.com>
2023-08-11 11:02:58 -0700

    Image from clipboard for Twitter
    Supported with -p flag to avoid accidental attachments.

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ import BskyAgent from '@atproto/api';
 import { Client } from '@threadsjs/threads.js';
 import Mastodon from 'mastodon-api';
 import { spawn } from 'child_process';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
 const argv = minimist(process.argv.slice(2));
 
@@ -24,6 +26,27 @@ if (argv.x || argv.b || argv.t || argv.m || argv.f) {
     farcaster = argv.f && farcaster;
 }
 
+let paste = argv.p;
+
+    let temp_image_file;
+if (paste) {
+    const __dirname = dirname(fileURLToPath(import.meta.url));
+    temp_image_file = `${__dirname}/.xclip_temp.png`;
+    let logStream = fs.createWriteStream(temp_image_file);
+
+    // TODO: support macOS and Windows
+    const xclipProcess = spawn('xclip', ['-selection', 'clipboard', '-t', 'image/png', '-o']);
+    xclipProcess.stdout.pipe(logStream);
+    await xclipProcess.on('exit', (code) => {
+        console.log(`xclip exited with code ${code}`);
+    });
+    function sleep(ms) {
+      return new Promise((resolve) => setTimeout(resolve, ms));
+    }
+    // I'm not sure why the previous await wasn't sufficient. In the future we can refactor the code as a callback.
+    await sleep(500);
+}
+
 if (x) {
     const consumerClient = new TwitterApi({
       appKey: x.appKey,
@@ -36,7 +59,15 @@ if (x) {
     for (const tweetText of argv._) {
         let x_response;
         if (!previous_tweet_id) {
-            x_response = await consumerClient.v2.tweet(tweetText);
+            if (paste) {
+                const mediaIds = await Promise.all([consumerClient.v1.uploadMedia(temp_image_file)]);
+                // TODO: can we refactor to pass empty mediaIds array when no images to only call tweet once?
+                x_response = await consumerClient.v2.tweet(
+                    {text: tweetText,
+                     media: { media_ids: mediaIds }});
+            } else {
+                x_response = await consumerClient.v2.tweet(tweetText);
+            }
         } else {
             x_response = await consumerClient.v2.reply(tweetText, previous_tweet_id);
         }
